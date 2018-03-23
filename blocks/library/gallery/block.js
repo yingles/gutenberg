35fdd72a5c552264c18902a24fdf7cf2351023c1
Joen Asmussen <asmussen@gmail.com>
2018-03-23 11:09:25 +0100

    Add condition for checked and unchecked labels
    Components using the FormToggle component can use this to supply help text based on the state of the checkbox.

--- blocks/library/gallery/block.js
+++ blocks/library/gallery/block.js
@@ -218,6 +218,8 @@ class GalleryBlock extends Component {
 						label={ __( 'Crop Images' ) }
 						checked={ !! imageCrop }
 						onChange={ this.toggleImageCrop }
+						help={ __( 'Thumbnails are not cropped.' ) }
+						checkedHelp={ __( 'Thumbnails are cropped to align.' ) }
 					/>
 					<SelectControl
 						label={ __( 'Link to' ) }
